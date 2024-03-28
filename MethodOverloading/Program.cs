e518e76965cbf4abeadbfba8e1ca193d071b626f
nwhang1 <nicholasjwhang@gmail.com>
2024-03-28 09:58:36 -0700

    created method overloads

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -6,5 +6,32 @@
         {
             Console.WriteLine("Hello, World!");
         }
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        
+        public static string Add(int x, int y, bool z)
+        {
+            if (z == true)
+            {
+                if (x + y > 1)
+                {
+                    return x + y + "dollars";
+                }
+                else
+                {
+                    return x + y + "dollar";
+                }          
+            }
+            else
+            {
+                return $"x + y";
+            }
+        }
     }
 }
